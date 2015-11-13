c5812a0d784c114b6a75617a04213207049fca81
gerbias <smitnich@onid.orst.edu>
2015-11-12 23:43:45 -0800

    Implement moving animations on item deletion

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import sys
 from pygame.locals import *
+from enum import IntEnum
 
 all_objects = None
 
@@ -12,17 +13,35 @@ sprite_paths = ['gfx/boulder.png', 'gfx/fire.png', 'gfx/ice.png']
 image_cache = []
 Gameboard = [[0 for x in range(gameboard_size)] for x in range(gameboard_size)] 
 
+class Game_State(IntEnum):
+    ready = 1
+    animation = 2
+    last_state = 3
+
+game_state = Game_State.ready
+anim_progress = 0.0
+
 class Element:
     x = 0
     y = 0
     index = 0
+    moving = False
     sprite = None
     def __init__(self, _index):
         self.index = _index
         self.sprite = image_cache[self.index]
 
     def draw(self,x,y):
-        screen.blit(self.sprite,(x*image_size, y*image_size))
+        global anim_progress
+        if self.moving:
+            screen.blit(self.sprite,(x*image_size, (y-1)*image_size+int(anim_progress)))
+        else:
+            screen.blit(self.sprite,(x*image_size, y*image_size))
+
+    def update(self):
+        global game_state
+        if game_state is not Game_State.animation:
+            self.moving = False
 
 def move_element(obj, x, y):
     global gameboard_size
@@ -32,6 +51,8 @@ def move_element(obj, x, y):
         print(x,y," is not None")
         return
     Gameboard[x][y] = obj
+    if obj is not None:
+        obj.moving = True
 
 def random_gameboard():
     global gameboard_size
@@ -44,13 +65,16 @@ def random_gameboard():
             obj.y = y*image_size
 
 def drop_column(x, y, count):
+    global gameboard_size
+    if x < 0 or y < 0 or x > gameboard_size or y > gameboard_size:
+        return
     delete_object(x,y)
     if Gameboard[x][y] is not None:
         return
     for i in range(y-1,-1,-1):
         move_element(Gameboard[x][i],x,i+count)
         delete_object(x,i)
-    
+    set_game_state(Game_State.animation)
             
 def delete_object(x,y):
     global Gameboard
@@ -92,6 +116,28 @@ def load_images():
     for path in sprite_paths:
         image_cache = image_cache + [pygame.image.load(path)]
 
+def do_animations():
+    global anim_progress
+    anim_progress += 1
+    if anim_progress >= image_size:
+        anim_progress = 0.0
+        set_game_state(Game_State.ready)
+    return
+
+def game_loop():
+    global game_state
+    if game_state == Game_State.ready:
+        poll_events()
+    elif game_state == Game_State.animation:
+        do_animations()
+        return
+
+def set_game_state(new_state):
+    global game_state
+    if new_state < 0 or new_state >= Game_State.last_state:
+        return
+    game_state = new_state
+
 def main():
     global screen
     screen = pygame.display.set_mode((1024, 768))
@@ -100,12 +146,13 @@ def main():
     random_gameboard()
     while True:
         screen.fill((0,0,0))
-        poll_events()
+        game_loop()
         for x in range(0,gameboard_size):
             for y in range(0, gameboard_size):
                 obj = Gameboard[x][y]
                 if (obj is not None):
-                    obj.draw(x, y);
+                    obj.update()
+                    obj.draw(x, y)
         pygame.display.flip()
 
 main()
