21fa37347a4fc3f890d05921a57d2fe626944f3b
gerbias <smitnich@onid.orst.edu>
2015-11-15 10:07:40 -0800

    Implement basic vertical match-making

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ all_objects = None
 gameboard_size = 10
 image_size = 48
 
+match_length = 3
 sprite_paths = ['gfx/boulder.png', 'gfx/fire.png', 'gfx/ice.png']
 image_cache = []
 Gameboard = [[0 for x in range(gameboard_size)] for x in range(gameboard_size)] 
@@ -16,14 +17,13 @@ Gameboard = [[0 for x in range(gameboard_size)] for x in range(gameboard_size)]
 class Game_State(IntEnum):
     ready = 1
     animation = 2
-    last_state = 3
+    update = 3
+    last_state = 4
 
 game_state = Game_State.ready
 anim_progress = 0.0
 
 class Element:
-    x = 0
-    y = 0
     index = 0
     moving = False
     sprite = None
@@ -68,12 +68,16 @@ def drop_column(x, y, count):
     global gameboard_size
     if x < 0 or y < 0 or x > gameboard_size or y > gameboard_size:
         return
-    delete_object(x,y)
+    for i in range(0,count):
+        delete_object(x,y+i)
     if Gameboard[x][y] is not None:
         return
-    for i in range(y-1,-1,-1):
+    for i in range(y-1, -count, -1):
         move_element(Gameboard[x][i],x,i+count)
         delete_object(x,i)
+    for i in range(0, count):
+        obj = random_object()
+        move_element(obj,x,i)
     set_game_state(Game_State.animation)
             
 def delete_object(x,y):
@@ -89,6 +93,28 @@ def create_objects():
     obj2 = Element(1)
     obj3 = Element(2)
     all_objects = [obj1, obj2, obj3]
+
+def check_adjacency(match_length):
+    global Gameboard
+    global gameboard_size
+    match_found = False
+    for x in range(0, gameboard_size):
+        match_count = 0
+        to_match = Gameboard[x][0].index
+        for y in range(0, gameboard_size):
+            if Gameboard[x][y].index == to_match:
+                match_count = match_count+1
+            else:
+                if match_count >= match_length:
+                    match_found = True
+                    drop_column(x, y-match_count, match_count)
+                match_count = 1
+            to_match = Gameboard[x][y].index
+        if match_count >= match_length:
+            match_found = True
+            drop_column(x, gameboard_size-match_count, match_count)
+    return match_found
+    
 
 def random_object():
     global all_objects
@@ -121,16 +147,18 @@ def do_animations():
     anim_progress += 1
     if anim_progress >= image_size:
         anim_progress = 0.0
-        set_game_state(Game_State.ready)
-    return
+        set_game_state(Game_State.update)
 
 def game_loop():
     global game_state
+    global match_length
     if game_state == Game_State.ready:
         poll_events()
     elif game_state == Game_State.animation:
         do_animations()
-        return
+    elif game_state == Game_State.update:
+        if not check_adjacency(match_length):
+            set_game_state(Game_State.ready)
 
 def set_game_state(new_state):
     global game_state
