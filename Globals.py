1d41f161b33a00294b24a6352117bbff39393a65
gerbias <smitnich@onid.orst.edu>
2016-02-14 19:11:37 -0800

    Update variables.

--- Globals.py
+++ Globals.py
@@ -4,29 +4,14 @@ class Game_State(IntEnum):
     ready = 1
     animation = 2
     update = 3
-    last_state = 4
+    game_over = 4
+    last_state = 5
 
 def init():
-    global image_size
-    global margin
-    global match_length
-    global sprite_paths
-    global image_cache
-    global all_objects
-    global selected_element
-    global background
-    global do_quit
-    global update_score
-    global cursor_pos
-    global game_state
-    global anim_progress
-    global seed_sprite
-    global growth_time
-    global current_turn
-    global poof_sprite
-    global current_ticks
-    global screenX
-    global screenY
+    global image_size, margin, match_length,sprite_paths,image_cache,all_objects,selected_element
+    global background,do_quit,update_score,cursor_pos,game_state,anim_progress,seed_sprite
+    global growth_time,current_turn,poof_sprite,current_ticks,screenX,screenY,clock,start_time
+    global do_new_game
     image_size = 64
     margin = (16, 16)
     match_length = 3
@@ -48,3 +33,6 @@ def init():
     current_ticks = 0
     screenX = 800
     screenY = 600
+    clock = None
+    start_time = 0
+    do_new_game = True
